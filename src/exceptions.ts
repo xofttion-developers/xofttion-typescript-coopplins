ef408fd421a5696d9ed9f773e531ec11b9d5ad44
ing.dacastillop <ing.dacastillop@gmail.com>
2023-02-09 11:50:26 -0500

    :sparkles: New handler response Controller

---
+++ src/exceptions.ts
@@ -0,0 +1,41 @@
+import { HttpCode } from './types';
+
+export class CoopplinsError extends Error {
+  constructor(
+    public readonly code: number,
+    message: string,
+    public readonly data?: any
+  ) {
+    super(message);
+  }
+}
+
+export class BadRequestError extends CoopplinsError {
+  constructor(message: string, data?: any) {
+    super(HttpCode.BadRequest, message, data);
+  }
+}
+
+export class UnauthorizedError extends CoopplinsError {
+  constructor(message: string, data?: any) {
+    super(HttpCode.Unauthorized, message, data);
+  }
+}
+
+export class ForbiddenError extends CoopplinsError {
+  constructor(message: string, data?: any) {
+    super(HttpCode.Forbidden, message, data);
+  }
+}
+
+export class NotFoundError extends CoopplinsError {
+  constructor(message: string, data?: any) {
+    super(HttpCode.NotFound, message, data);
+  }
+}
+
+export class InternalServerError extends CoopplinsError {
+  constructor(message: string, data?: any) {
+    super(HttpCode.InternalServerError, message, data);
+  }
+}
