c15f5607dce93bd2364c8eaa78363c96967cf911
ing.dacastillop <ing.dacastillop@gmail.com>
2023-07-12 09:50:13 -0500

    :bento: Export class Result

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export * from './decorators';
 export * from './exceptions';
 export * from './results';
 export { coopplins, environment, validator } from './server';
-export { Http, HttpCode, OnMiddleware, ResultServer } from './types';
+export { Http, HttpCode, OnMiddleware, Result, ResultServer } from './types';
